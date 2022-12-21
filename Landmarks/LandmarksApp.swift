2bdebaa78e1b41e9fbdda370811aaf424391879f
Kevin Blanchard <blanchardkevinj@gmail.com>
2022-12-21 10:25:26 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kevin Blanchard on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
